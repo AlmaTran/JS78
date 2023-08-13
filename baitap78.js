ae533cdf380f701ec9ae6afba7617d0711cf693f
ducthuan109 <ducthuan1022@gmail.com>
2023-08-13 14:15:07 +0700

    update baitap

--- baitap78.js
+++ baitap78.js
@@ -1,85 +1,162 @@
-function calculateSum() {
-    const input = document.getElementById("integerInput").value;
-    const numbers = input.split(',').map(Number);
-    
-    let sum = 0;
-    for (let i = 0; i < numbers.length; i++) {
-      if (!isNaN(numbers[i])) {
-        sum += numbers[i];
-      }
+var integerArray = [];
+
+function addToIntegerArray() {
+    var inputElement = document.getElementById("integerInput");
+    var inputValue = inputElement.value;
+    var integerValue = parseInt(inputValue);
+
+    if (!isNaN(integerValue)) {
+        integerArray.push(integerValue);
+        inputElement.value = ""; // Clear the input field
+        updateArrayDisplay();
+    }
+}
+
+function updateArrayDisplay() {
+    var arrayDisplayElement = document.getElementById("arrayDisplay");
+    arrayDisplayElement.innerHTML = "Mảng số nguyên: " + integerArray.join(", ");
+}
+//tính tổng các số dương
+
+
+function updateArrayDisplay() {
+    var arrayDisplayElement = document.getElementById("arrayDisplay");
+    arrayDisplayElement.innerHTML = "Mảng số nguyên: " + integerArray.join(", ");
+}
+
+function calculateSumOfPositiveNumbers() {
+    var sum = 0;
+    for (var i = 0; i < integerArray.length; i++) {
+        if (integerArray[i] > 0) {
+            sum += integerArray[i];
+        }
+    }
+    var sumDisplayElement = document.getElementById("sumDisplay");
+    sumDisplayElement.innerHTML = "Tổng số dương: " + sum;
+}
+
+//đếm số lượng số dương
+
+
+function updateArrayDisplay() {
+    var arrayDisplayElement = document.getElementById("arrayDisplay");
+    arrayDisplayElement.innerHTML = "Mảng số nguyên: " + integerArray.join(", ");
+}
+
+function countPositive() {
+    var positiveCount = 0;
+    for (var i = 0; i < integerArray.length; i++) {
+        if (integerArray[i] > 0) {
+            positiveCount++;
+        }
+    }
+    var countPositiveInput = document.getElementById("countPositiveInput");
+    countPositiveInput.value = positiveCount;
+
+    var resultElement = document.getElementById("result");
+    resultElement.innerHTML = "Số lượng số dương: " + positiveCount;
+}
+//tìm số nhỏ nhất trong mảng
+function updateArrayDisplay() {
+    var arrayDisplayElement = document.getElementById("arrayDisplay");
+    arrayDisplayElement.innerHTML = "Mảng số nguyên: " + integerArray.join(", ");
+}
+
+function findMin() {
+    if (integerArray.length === 0) {
+        return;
     }
 
-    document.getElementById("result").innerHTML = "Tổng các số nguyên là: " + sum;
-  }
-
-  function countPositive() {
-    const input = document.getElementById("integerInput").value;
-    const numbers = input.split(",").map(Number);
-    const positiveCount = numbers.filter(num => num > 0).length;
-    document.getElementById("countPositiveInput").value = positiveCount;
-  }
-
-  function findMin() {
-    const input = document.getElementById("integerInput").value;
-    const numbers = input.split(",").map(Number);
-    const min = Math.min(...numbers);
-    document.getElementById("minInput").value = min;
-  }
-  function calculateSum() {
-    const input = document.getElementById("integerInput").value;
-    const numbers = input.split(",").map(Number);
-    const sum = numbers.reduce((acc, num) => num > 0 ? acc + num : acc, 0);
-    document.getElementById("result").innerHTML = "Tổng các số dương: " + sum;
-  }
-
-  function countPositive() {
-    const input = document.getElementById("integerInput").value;
-    const numbers = input.split(",").map(Number);
-    const positiveCount = numbers.filter(num => num > 0).length;
-    document.getElementById("countPositiveInput").value = positiveCount;
-  }
-
-  function findMin() {
-    const input = document.getElementById("integerInput").value;
-    const numbers = input.split(",").map(Number);
-    const min = Math.min(...numbers);
-    document.getElementById("minInput").value = min;
-  }
-
-  function findMinPositive() {
-    const input = document.getElementById("integerInput").value;
-    const numbers = input.split(",").map(Number);
-    const positiveNumbers = numbers.filter(num => num > 0);
-    const minPositive = Math.min(...positiveNumbers);
-    document.getElementById("minPositiveInput").value = minPositive;
-  }
-
-  function findLastEven() {
-    const input = document.getElementById("integerInput").value;
-    const numbers = input.split(",").map(Number);
-    const evenNumbers = numbers.filter(num => num % 2 === 0);
-    const lastEven = evenNumbers.length > 0 ? evenNumbers[evenNumbers.length - 1] : -1;
-    document.getElementById("lastEvenInput").value = lastEven;
-  }
-
-  function swapValues() {
-    const input = document.getElementById("integerInput").value;
-    const numbers = input.split(",").map(Number);
-    const pos1 = Number(document.getElementById("swapInput1").value);
-    const pos2 = Number(document.getElementById("swapInput2").value);
-
-    if (pos1 >= 0 && pos1 < numbers.length && pos2 >= 0 && pos2 < numbers.length) {
-      [numbers[pos1], numbers[pos2]] = [numbers[pos2], numbers[pos1]];
-      document.getElementById("result").innerHTML = "Đã đổi chỗ giá trị.";
-    } else {
-      document.getElementById("result").innerHTML = "Vị trí không hợp lệ.";
+    var min = integerArray[0];
+    for (var i = 1; i < integerArray.length; i++) {
+        if (integerArray[i] < min) {
+            min = integerArray[i];
+        }
     }
-  }
+    var minInput = document.getElementById("minInput");
+    minInput.value = min;
+}
+//tìm số dương nhỏ nhất trong mảng
+function updateArrayDisplay() {
+    var arrayDisplayElement = document.getElementById("arrayDisplay");
+    arrayDisplayElement.innerHTML = "Mảng số nguyên: " + integerArray.join(", ");
+}
 
-  function sortArray() {
-    const input = document.getElementById("integerInput").value;
-    const numbers = input.split(",").map(Number);
-    numbers.sort((a, b) => a - b);
-    document.getElementById("sortInput").value = numbers.join(",");
-  }
-  
+function findMinPositive() {
+    var minPositive = -1;
+    for (var i = 0; i < integerArray.length; i++) {
+        if (integerArray[i] > 0 && (minPositive === -1 || integerArray[i] < minPositive)) {
+            minPositive = integerArray[i];
+        }
+    }
+    var minPositiveInput = document.getElementById("minPositiveInput");
+    minPositiveInput.value = minPositive;
+}
+// tìm sô chẵn cuối cùng
+function updateArrayDisplay() {
+    var arrayDisplayElement = document.getElementById("arrayDisplay");
+    arrayDisplayElement.innerHTML = "Mảng số nguyên: " + integerArray.join(", ");
+}
+
+function findLastEven() {
+    var lastEven = -1;
+    for (var i = integerArray.length - 1; i >= 0; i--) {
+        if (integerArray[i] % 2 === 0) {
+            lastEven = integerArray[i];
+            break;
+        }
+    }
+    var lastEvenInput = document.getElementById("lastEvenInput");
+    lastEvenInput.value = lastEven;
+}
+// sắp xếp mảng tăng dần
+function updateArrayDisplay() {
+    var arrayDisplayElement = document.getElementById("arrayDisplay");
+    arrayDisplayElement.innerHTML = "Mảng số nguyên: " + integerArray.join(", ");
+}
+
+function sortArray() {
+    integerArray.sort(function(a, b) {
+        return a - b;
+    });
+    var sortInput = document.getElementById("sortInput");
+    sortInput.value = integerArray.join(", ");
+}
+// dổi chỗ
+function swapValues() {
+    var swapInput1 = document.getElementById("swapInput1");
+    var swapInput2 = document.getElementById("swapInput2");
+    var position1 = parseInt(swapInput1.value);
+    var position2 = parseInt(swapInput2.value);
+
+    if (!isNaN(position1) && !isNaN(position2) && position1 >= 0 && position2 >= 0 && position1 < integerArray.length && position2 < integerArray.length) {
+        var temp = integerArray[position1];
+        integerArray[position1] = integerArray[position2];
+        integerArray[position2] = temp;
+        updateArrayDisplay();
+    }
+}
+// tìm số nguyên tố
+function isPrime(number) {
+    if (number <= 1) {
+        return false;
+    }
+    for (var i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function findFirstPrime() {
+    var firstPrime = null;
+    for (var i = 0; i < integerArray.length; i++) {
+        if (isPrime(integerArray[i])) {
+            firstPrime = integerArray[i];
+            break;
+        }
+    }
+    var primeInput = document.getElementById("primeInput");
+    primeInput.value = firstPrime !== null ? firstPrime : "Không có số nguyên tố";
+}
